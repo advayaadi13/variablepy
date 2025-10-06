a67557afd7468c5e8683f4a6ecba4e9c4e3caaa6
advayaadi13 <advayaadi11@gmail.com>
2025-10-06 17:44:45 +1300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,73 @@
+name = "Aadi"
+age = 13
+weight = 50.5
+is_student = True
+
+print("Data type of name is", type(name))
+print("Data type of age is", type(age))
+print("Data type of weight is ",type(weight))
+print("Data type of is_student is ",type(is_student))
+age = str(age)
+print("Data type of age is", type(age))
+weight = int(weight)
+print("data type of weight is", type(weight))
+
+
+num1 = 56
+num2 = 4
+
+print("Number 1 :", num1)
+print("Number 2 :", num2)
+print("Addition :", num1+num2)
+print("Difference :", num1-num2)
+print("Product :", num1*num2)
+print("Division :", num1/num2)
+print("Floor Division :", num1//num2)
+print("Modulus Operation :", num1%num2)
+print("Square :", num1**num2)
+print("Square Root :", num1**0.5)
+
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1>num2)
+print("Number 2 greater?", num1<num2)
+print("Not Equal?", num1!=num2)
+
+result = num1/num2+num2**2+10
+print("Result of Given Equation is", result)
+
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String multiplied by 5 gives this result :", example)
+
+word = "Coding"
+print("Length of String :", len(word))
+print("First Letter of the String :", word[0])
+print("Last Letter of the String :", word[5])
+print("String Sliced :", word[0:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
