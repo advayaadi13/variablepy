34aad6fcbd11df9420fab27e984f9ece5402f8b4
advayaadi13 <advayaadi11@gmail.com>
2026-04-09 16:49:33 +1200

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,52 @@
+from tkinter import tk
+from tkinter import messagebox
+
+
+def info_box():
+    messagebox.showinfo("Information", "This is an important message.")
+
+def warning_box():
+    messagebox.showwarning("Warning", "This is a warning message!")
+
+def error_box():
+    messagebox.showerror("Error", "An error has occured!")
+
+def question_box():
+    response = messagebox.askquestion("Question", "Do you like Python?")
+    print("Response", response)     
+
+def ok_cancel_box():
+    response = messagebox.askokcancel("Confirm", "Do you want to proceed?")
+    print("Response", response)
+
+def yes_no_box():
+    response = messagebox.askyesno("Confirm", "Do you want to save changes?")
+    print("Response", response)
+
+def retry_cancel_box():
+    response = messagebox.askretrycancel("Retry", "Operation failed. Retry?")
+    print("Response", response)
+
+root = tk.Tk()
+
+root.title("Message Box Examples")
+
+root.geometry("300x400")
+
+# Buttons for each message box
+
+tk.Button(root, text="Show Info", command=info_box).pack(pady=5)
+
+tk.Button(root, text="Show Warning", command=warning_box).pack(pady=5)
+
+tk.Button(root, text="Show Error", command=error_box).pack(pady=5)
+
+tk.Button(root, text="Ask Question", command=question_box).pack(pady=5)
+
+tk.Button(root, text="Ask Ok Cancel", command=ok_cancel_box).pack(pady=5)
+
+tk.Button(root, text="Ask Yes No", command=yes_no_box).pack(pady=5)
+
+tk.Button(root, text="Ask Retry Cancel", command=retry_cancel_box).pack(pady=5)
+
+root.mainloop()                      
