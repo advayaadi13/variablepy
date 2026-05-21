2a73f74c3fb35349f0012e431e5320b9d5eaa1ea
advayaadi13 <advayaadi11@gmail.com>
2026-05-21 16:35:55 +1200

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,31 @@
+def prints(n):
+    if n <= 0:
+        return
+    
+    print("Codingal")
+    prints(n // 2)
+    prints(n // 2)
+
+prints(8)
+
+def sum_n(n):
+    return n * (n + 1) // 2
+
+print("Sum of first n numbers:(n=5):", sum_n(5))
+
+def array_sum(a):
+    total = 0
+    for i in a:
+        total += i
+    return total
+
+a = [12, 3, 4, 15]
+print("Array sum:", array_sum(a))
+
+def sumn(n):
+    if n <= 0:
+        return 0
+    return n + sumn(n - 1)
+
+print("Recursive sum (n=5):", sumn(5))
+
